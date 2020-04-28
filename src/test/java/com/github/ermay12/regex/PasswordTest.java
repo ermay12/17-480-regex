d40f50d831d9843dfdebf4d662264029ffb9f546
Eric Maynard <ermay12@gmail.com>
2020-04-28 14:01:51 -0400

    rethought how some of the api should look

--- src/test/java/com/github/ermay12/regex/PasswordTest.java
+++ src/test/java/com/github/ermay12/regex/PasswordTest.java
@@ -2,12 +2,44 @@ package com.github.ermay12.regex;
 
 import org.junit.Test;
 
+
 import static com.github.ermay12.regex.RegexBuilder.StaticHelpers.*;
 import static org.junit.Assert.assertEquals;
 
 public class PasswordTest {
     @Test
     public void testPassword() {
+
+        //Without importing everything
+        Regex regex =
+            new Regex(
+                Regex.LINESTART,
+                Regex.lookahead(Charset.WILDCARD.anyAmount(), Charset.range('a', 'z')),
+                Regex.lookahead(Charset.WILDCARD.anyAmount(), Charset.range('A', 'Z')),
+                Regex.lookahead(Charset.WILDCARD.anyAmount(), Charset.DIGIT),
+                Regex.lookahead(Charset.WILDCARD.anyAmount(), Charset.without(Charset.WORD_CHAR)),
+                Charset.WILDCARD.repeatAtLeast(8),
+                Regex.LINEEND
+            );
+
+        // importing everything
+        import static com.github.ermay12.regex.Regex.*;
+        import static com.github.ermay12.regex.Charset.*;
+        import com.github.ermay12.regex.Regex;
+        import com.github.ermay12.regex.Charset;
+
+
+        Regex regex =
+            new Regex(
+                LINESTART,
+                lookahead(WILDCARD.anyAmount(), range('a', 'z')),
+                lookahead(WILDCARD.anyAmount(), range('A', 'Z')),
+                lookahead(WILDCARD.anyAmount(), DIGIT),
+                lookahead(WILDCARD.anyAmount(), Charset.without(WORD_CHAR)),
+                WILDCARD.repeatAtLeast(8),
+                LINEEND
+            );
+
         Regex regex =
                 startLine()
                 .lookahead(anyAmount(wildcard()).single(range('a', 'z')))
--- src/test/java/com/github/ermay12/regex/YoutubeTest.java
+++ src/test/java/com/github/ermay12/regex/YoutubeTest.java
@@ -8,7 +8,56 @@ import static org.junit.Assert.assertEquals;
 public class YoutubeTest {
     @Test
     public void youtubeTest() {
-        Regex regex =
+
+      //without static imports
+      RegexGroup group = new Regex(
+          Charset.without('#', '&', '?').anyAmount()
+      );
+
+      Regex regex =
+          new Regex(
+              Regex.LINESTART,
+              Charset.WILDCARD.anyAmount(),
+              Regex.oneOf(
+                  Regex.string("youtu.be/"),
+                  Regex.string("v/"),
+                  Regex.string("/u/w/"),
+                  Regex.string("embed/"),
+                  Regex.string("watch?")
+              ),
+              Regex.optional("?"),
+              Regex.optional("v"),
+              Regex.optional("="),
+              group,
+              Regex.string("asdasd").anyAmount()
+          );
+
+
+
+      //with static imports
+      RegexGroup group = new Regex(
+          Charset.without('#', '&', '?').anyAmount()
+      );
+
+      Regex regex =
+          new Regex(
+              LINESTART,
+              WILDCARD.anyAmount(),
+              oneOf(
+                  string("youtu.be/"),
+                  string("v/"),
+                  string("/u/w/"),
+                  string("embed/"),
+                  string("watch?")
+              ),
+              optional("?"),
+              optional("v"),
+              optional("="),
+              group,
+              string("asdasd").anyAmount()
+          );
+
+      Regex regex =
                 startLine()
                 .anyAmount(wildcard())
                 .or(
