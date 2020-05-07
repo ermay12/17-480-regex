60011f4393377685b9820a9bda6911f36a48a300
Cal Lavicka <cal.lavicka@gmail.com>
2020-05-07 11:29:45 -0400

    Added index documentation

--- src/main/java/com/github/ermay12/regex/CapturingGroup.java
+++ src/main/java/com/github/ermay12/regex/CapturingGroup.java
@@ -3,10 +3,10 @@ package com.github.ermay12.regex;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- * A CapturingGroup represents a subsection of a larger Regex which you can extract matches from.
+ * <p> A CapturingGroup represents a subsection of a larger Regex which you can extract matches from.
  * For instance, if you wanted a regex to match an email such as "test@example.com", you could use
  * two CapturingGroups to "capture" both the name (test) and the domain (example.com) and separate that
- * out. An example of this is:
+ * out. An example of this is: </p>
  * <pre>
  *     CapturingGroup nameGroup = CapturingGroup.capture(anyAmount(not('@'));
  *     CapturingGroup domainGroup = CapturingGroup.capture(anyAmount(not('.')), string("."), anyAmount(WILDCARD));
@@ -17,10 +17,37 @@ import java.util.concurrent.atomic.AtomicLong;
  *     result.getGroup(nameGroup); // "test"
  *     result.getGroup(emailGroup); // "example.com"
  * </pre>
- *
- * If the same CapturingGroup is present in a regex multiple times, the last match will be used as the value.
+ * <p> If the same CapturingGroup is present in a regex multiple times, the last match will be used as the value.
  * This also applies, for instance, if the CapturingGroup is in an "anyAmount", as the last match from that
- * will be the value.
+ * will be the value. </p>
+ *
+ * <p> Capturing groups are numbered by counting their opening parentheses from
+ *  left to right.  In the expression </p>
+ *  <pre>
+ capturing(
+    capturing(string("A")),
+    capturing(
+        string("B"),
+        capturing(string("C")
+    )
+ )
+ </pre>
+ * <p> there are four such groups: </p>
+ *
+ *  <blockquote><table><caption>Capturing group numberings</caption>
+ *  <tbody><tr><th>1&nbsp;&nbsp;&nbsp;&nbsp;</th>
+ *      <td>The whole group</td></tr>
+ *  <tr><th>2&nbsp;&nbsp;&nbsp;&nbsp;</th>
+ *      <td>capturing(string("A"))</td></tr>
+ *  <tr><th>3&nbsp;&nbsp;&nbsp;&nbsp;</th>
+ *      <td>capturing(concatenate(string("B"), capturing(string("C"))))
+ *  <tr><th>4&nbsp;&nbsp;&nbsp;&nbsp;</th>
+ *      <td>capturing(string("C"))</td></tr>
+ *  </tbody></table></blockquote>
+ *  <p> Group zero always stands for the entire expression.
+ *  </p>
+ *  <p>There are some methods which can refer to CapturingGroups via this index, however passing in a reference
+ *  to the group itself should almost always be preferred to those methods.</p>
  */
 public final class CapturingGroup extends Regex {
 
