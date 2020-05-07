160d1a85676573d702e66590f3aae8ead3058242
Alex Stanescu <astanesc@andrew.cmu.edu>
2020-05-06 20:44:38 -0400

    Added method-level documentation to character class

--- src/main/java/com/github/ermay12/regex/CharacterClass.java
+++ src/main/java/com/github/ermay12/regex/CharacterClass.java
@@ -73,6 +73,10 @@ package com.github.ermay12.regex;
  *  </table>
  */
 public class CharacterClass extends Regex {
+    /**
+     * Creates a new character class from the given components
+     * @param components the components to create the character class from
+     */
     private CharacterClass(String... components) {
         super(components);
     }
@@ -82,20 +86,53 @@ public class CharacterClass extends Regex {
         return getRawRegex();
     }
 
+    /**
+     * A character class representing any character possible
+     */
     public static final CharacterClass WILDCARD = new CharacterClass(".");
+
+    /**
+     * A character class representing any digit. Equivalent to range('0', '9')
+     */
     public static final CharacterClass DIGIT = new CharacterClass("\\d");
+
+    /**
+     * A character representing any character that is either alphanumeric or '_'. Equivalent to
+     * union(range('a', 'z'), range('A', 'Z'), DIGIT, character('_'))
+     */
     public static final CharacterClass WORD_CHARACTER = new CharacterClass("\\w");
+
+    /**
+     * Any whitespace character (e.g. one of  ,\t,\n,\x0B,\f, or \r
+     */
     public static final CharacterClass WHITESPACE = new CharacterClass("\\s");
 
+    /**
+     * Returns a character class recognizing only the given character
+     * @param c the character to recognize
+     * @return a character class recognizing only the given character
+     */
     public static CharacterClass character(char c) {
         return new CharacterClass(sanitized(c));
     }
 
+    /**
+     * Returns a character class containing all character whose ASCII values lie
+     * between c1 and c2. If a character does not have an ascii value (e.g. unicode characters),
+     * the raw bytes representing that character are used instead.
+     * @param c1 The starting character of the range
+     * @param c2 The ending character of the range
+     * @return a character class containing all character whose ASCII values lies between c1 and c2
+     */
     public static CharacterClass range(char c1, char c2) {
         return new CharacterClass("[", sanitized(c1), "-", sanitized(c2), "]");
     }
 
-    //TODO(astanesc): More rigorous implementation!
+    /**
+     * Returns a character class containing all characters that are not in the given character class
+     * @param c A character class
+     * @return a character class containing all characters that are not in the given character class
+     */
     public static CharacterClass not(CharacterClass c) {
         if(c.equals(WILDCARD)){
             return new CharacterClass("[\\n]");
@@ -117,6 +154,11 @@ public class CharacterClass extends Regex {
         }
     }
 
+    /**
+     * Returns a character class containing all of the characters passed in
+     * @param choices the characters that the given character class should be made of
+     * @return a character class containing all of the characters passed in
+     */
     public static CharacterClass oneOf(char... choices) {
         StringBuilder regex = new StringBuilder();
         regex.append("[");
@@ -127,6 +169,12 @@ public class CharacterClass extends Regex {
         return new CharacterClass(regex.toString());
     }
 
+    /**
+     * Returns a character class containing all characters found in any of the input character classes.
+     * Equivalent to a union operation on sets.
+     * @param choices the input character classes
+     * @return a character class containing all characters found in any of the input character classes.
+     */
     public static CharacterClass union(CharacterClass... choices) {
         if(choices.length > 1) {
             StringBuilder regex = new StringBuilder();
@@ -142,6 +190,14 @@ public class CharacterClass extends Regex {
             return new CharacterClass("[]");
         }
     }
+
+
+    /**
+     * Returns a character class containing all characters found in all of the input character classes.
+     * Equivalent to an intersect operation on sets.
+     * @param choices the input character classes
+     * @return a character class containing all characters found in all of the input character classes.
+     */
     public static CharacterClass intersection(CharacterClass... choices) {
         if(choices.length > 1) {
             StringBuilder regex = new StringBuilder();
