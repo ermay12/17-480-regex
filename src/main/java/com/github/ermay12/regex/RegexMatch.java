bc393b79d2978794c975307ebc2281ff7f5e8244
Cal Lavicka <cal.lavicka@gmail.com>
2020-05-09 15:38:11 -0400

    Added documentation for RegexMatch

--- src/main/java/com/github/ermay12/regex/RegexMatch.java
+++ src/main/java/com/github/ermay12/regex/RegexMatch.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 
+/**
+ * A RegexMatch represents a single match on a String by a regex. It allows you to get
+ * the string of the match, as well as the index in the entire string. It also allows youo
+ * to extract the individual capturing groups that are in the regex, either by passing the group
+ * in or using the index of the group (see: {@link CapturingGroup} for information on group indexes).
+ *
+ * Each RegexMatch also has a match index associated with it, which is telling which match in the
+ * original string this is.
+ */
 public class RegexMatch {
   String matchString;
   RegexLiteral regex;
@@ -21,11 +30,19 @@ public class RegexMatch {
     this.index = index;
   }
 
-
+  /**
+   * Gets the substring of the original String that was matched
+   * @return The substring
+   */
   public String getMatchString() {
     return matchString;
   }
 
+  /**
+   * Gets the substring matched by the given group in this match.
+   * @param group The group to grab.
+   * @return The string matched by the group.
+   */
   public String getGroup(CapturingGroup group) {
     if (!this.regex.groupToIndex.containsKey(group)) {
       throw new IllegalArgumentException("Group passed in is not present in regex!");
@@ -33,12 +50,21 @@ public class RegexMatch {
     return groups.get(this.regex.groupToIndex.get(group));
   }
 
-  public int getIndex() {
-    return index;
-  }
-
+  /**
+   * Gets the substring matched by the specific group number.
+   * @param number The numbered group to get. 0 is the entire match.
+   * @return The substring matched by that number.
+   */
   public String getGroup(int number) {
     return this.groups.get(number);
+  }
+
+  /**
+   * Gets the index of the match in the larger string.
+   * @return The index of this match, starting at 0.
+   */
+  public int getIndex() {
+    return index;
   }
 
   @Override
